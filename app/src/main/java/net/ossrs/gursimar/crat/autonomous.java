70a9d8500e0df445580b639c9683b20db864cfb5
Gursimar Ahluwalia <ahlu22@manukaumail.com>
2018-11-23 11:46:57 +1300

    autonomous activity created

---
+++ app/src/main/java/net/ossrs/gursimar/crat/autonomous.java
@@ -0,0 +1,14 @@
+package net.ossrs.gursimar.crat;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class autonomous extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autonomous);
+    }
+
+}
